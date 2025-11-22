5920aa234e0a969d14cc5d86b5f1f58b236293b1
Oceacarium <benfart22@gmail.com>
2025-11-21 18:03:41 -0600

    preliminary sd card writer

--- src/file_management.cpp
+++ src/file_management.cpp
@@ -10,13 +10,17 @@ inline namespace v5 {
             std::string data;
         public:
 
-
-
         File_management(char iaddress[]){
             memcpy(&address[1], &iaddress[1], sizeof(iaddress));
         }
         
-        void write(){
+        void write(std::string idata[]){
+            for(int i = 0; i>=sizeof(idata)-1; i++){
+                data += (idata[i] + ", ");
+            }
+        }
+
+        void save(){
             FILE*SD = fopen(address, "w");
             fwrite(&data, sizeof(data), sizeof(data), SD);
         }
